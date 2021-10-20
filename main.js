9674bdcdd42c635a7c44f1819c1546d0be8ea7a2
Mohamad Al moualem <m.almouallem@gmail.com>
2021-10-20 18:12:18 +0200

    Add feature for stripping `v` prefix (#12)

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ const core = require("@actions/core")
 async function main() {
   try {
     const ref = process.env.GITHUB_REF
+    const strip_v = process.env.INPUT_STRIP_V
     if(!ref)
       throw "GITHUB_REF is not defined"
     if(!ref.startsWith("refs/tags/"))
       throw `Not a tag ref (${ref})`
-    const tag = ref.replace(/^refs\/tags\//, "")
+    let tag = ref.replace(/^refs\/tags\//, "")
+    
+    if(strip_v === "true" && tag.startsWith("v"))
+      tag = tag.replace(/^v/, "")
+
 
     core.info(`ref=${ref}`)
     core.info(`tag=${tag}`)
